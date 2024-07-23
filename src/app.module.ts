38ff0f62d786b34cd49ebea17c551cbcc6d651c8
Kimsungmin2 <anrl8913@naver.com>
2024-07-23 18:32:29 +0900

    feat:message start

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClanBoardsModule } from './clan-boards/clan-boards.module';
 import { S3Module } from './s3/s3.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [ClanBoardsModule, S3Module],
+  imports: [ClanBoardsModule, S3Module, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
