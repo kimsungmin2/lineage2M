f9d989ad4a3dd9d0939b50f3539b6a38918c5c5f
Kimsungmin2 <anrl8913@naver.com>
2024-08-01 18:14:56 +0900

    Feat:swagger Module

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './utils/filter/http-exception.filter';
+import { setupSwagger } from './utils/middleware/swagger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  const config = new DocumentBuilder()
-    .setTitle('duogg API')
-    .setDescription('duogg 개발을 위한 API 문서입니다.')
-    .setVersion('1.0')
-    .addCookieAuth('connect.sid')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('api', app, document);
+  setupSwagger(app);
   app.use(cookieParser());
   const port = process.env.PORT || 3000;
   await app.listen(port);
